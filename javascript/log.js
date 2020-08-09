d63735131930670c31cee5c2feca7345002dda15
jay-241 <68777397+jay-241@users.noreply.github.com>
2020-08-09 22:40:21 +0530

    Add files via upload

---
+++ javascript/log.js
@@ -0,0 +1,23 @@
+function validate()
+{
+            var username = document.getElementsByName("uname");
+            var password = document.getElementsByName("psw");
+
+            if(username[0].value=="jay241" && password[0].value=="123456")
+            {
+                alert("Welcome!!! Correct Usename and Password");
+                window.location.replace("https://jay-241.github.io/index.html");
+            }
+            else if(username[0].value=="jay241" && password[0].value!="123456")
+            {
+                alert("Invalid Password"); 
+            }
+            else if(username[0].value!="jay241" && password[0].value=="123456")
+            {
+                alert("Invalid Username"); 
+            }
+            else
+            {
+                alert("Invalid User Name and Password");
+            }
+}
